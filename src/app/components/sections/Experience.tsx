39673ccf0c654ce3ff989d2c552103fae6e04253
Ihor Oksentiuk <winbka.lem@gmail.com>
2024-01-15 15:15:36 +0200

    Fixed hover and created var for companyLogo item

--- src/app/components/sections/Experience.tsx
+++ src/app/components/sections/Experience.tsx
@@ -6,6 +6,8 @@ import gidLogo from '../../../assets/gid-logo.png'
 import { SectionTitle } from '@/app/shared/SectionTitle'
 
 export const Experience = () => {
+  const companyLogoStyles = "w-[140px] h-[80px] mb-[40px] md:mb-[0] hover:scale-125 duration-300 ease-in-out";
+
   return (
     <section
       className="border-b border-white border-opacity-50"
@@ -39,14 +41,14 @@ export const Experience = () => {
               </div>
 
               <a
-                className="transition-transform transform hover:scale-125 duration-300 ease-in-out"
+                className="transition-transform transform"
                 href={job.link}
                 target='blank_'
               >
                 {job.logo === 'FaGithubSquare' ? (
 
                   <FaGithubSquare
-                    className="w-[100px] h-[80px] mb-[40px] md:mb-[0]"
+                    className={companyLogoStyles}
                   />
 
                 ) : job.logo === 'gidLogo' ? (
@@ -54,7 +56,7 @@ export const Experience = () => {
                   <Image
                     src={gidLogo}
                     alt="gid-logo"
-                    className="w-[140px] h-[80px] mb-[40px] md:mb-[0]"
+                    className={companyLogoStyles}
                   />
 
                 ) : null}
